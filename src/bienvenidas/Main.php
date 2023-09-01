78b54818192e13ec208295c693999da2ef447d4d
zFrozead0s <141191707+zFrozead0s@users.noreply.github.com>
2023-09-01 17:29:39 +0200

    Actualizar Main.php

--- src/bienvenidas/Main.php
+++ src/bienvenidas/Main.php
@@ -1,1 +1,29 @@
 
+<?php  
+  
+ namespace bienvenidas/main;  
+  
+ use pocketmine\plugin\PluginBase;  
+ use pocketmine\event\Listener;  
+ use pocketmine\utils\TextFormat;  
+ use pocketmine\event\player\PlayerJoinEvent;  
+  
+ class Main extends PluginBase implements Listener{  
+    public function onLoad() {  
+                $this->getLogger()->info("§aThis plugin is BienvenidasV# plugin.");  
+                  $this->getLogger()->info("§cMade By Frozeado.");  
+          }  
+          public function onEnable() {  
+                  $this->getLogger()->info("§dBienvenidadV2 has been enabled.");  
+                  $this->getLogger()->info("§cThis plugin's License is the MIT-.");  
+                  $this->getServer()->getPluginManager()->registerEvents($this,$this);  
+          }  
+  
+        public function JoinEvent(PlayerJoinEvent $event) {  
+        $player = $event->getPlayer(); 
+        $event->setJoinMessage (TextFormat::RED . " [" . $player->getName() . "] §cHas joined to the server! "); 
+      }  
+  }  
+  
+  
+ ?> 
