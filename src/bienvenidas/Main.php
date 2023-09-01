61565805a4b4247e64120d3c121852274fe3c5c0
zFrozead0s <141191707+zFrozead0s@users.noreply.github.com>
2023-09-01 19:52:56 +0200

    Update Main.php

--- src/bienvenidas/Main.php
+++ src/bienvenidas/Main.php
@@ -1,25 +1,26 @@
 <?php  
+    
+namespace bienvenidas;
+
+use pocketmine\player\Player;
+use pocketmine\Server;
+use pocketmine\plugin\PluginBase;  
+use pocketmine\event\Listener;  
+use pocketmine\utils\TextFormat;  
+use pocketmine\event\player\PlayerJoinEvent;  
   
- namespace main;  
-  
- use pocketmine\plugin\PluginBase;  
- use pocketmine\event\Listener;  
- use pocketmine\utils\TextFormat;  
- use pocketmine\event\player\PlayerJoinEvent;  
-  
- class Main extends PluginBase implements Listener{  
-    public function onLoad() {  
-                $this->getLogger()->info("§aThis plugin is BienvenidasV# plugin.");  
-                  $this->getLogger()->info("§cMade By Frozeado.");  
-          }  
-          public function onEnable() {  
-                  $this->getLogger()->info("§dBienvenidadV2 has been enabled.");  
-                  $this->getLogger()->info("§cThis plugin's License is the MIT-License.");  
-                  $this->getServer()->getPluginManager()->registerEvents($this,$this);  
-          }  
-  
-        public function JoinEvent(PlayerJoinEvent $event) {  
-        $player = $event->getPlayer(); 
-        $event->setJoinMessage (TextFormat::RED . " [" . $player->getName() . "] §cHas joined to the server! "); 
-      }  
+class Main extends PluginBase implements Listener {
+
+  public function onEnable() : void {
+    $this->getServer()->getPluginManager()->registerEvents($this,$this); 
+    $this->getLogger()->info("§dBienvenidasV2 has been enabled.");  
+    $this->getLogger()->info("§cThis plugin's License is the MIT-License.");   
   }  
+  
+  public function JoinEvent(PlayerJoinEvent $event) {  
+    $player = $event->getPlayer(); 
+    $event->setJoinMessage (TextFormat::RED . " [" . $player->getName() . "] §cHas joined to the server! "); 
+  }
+
+}  
+  
