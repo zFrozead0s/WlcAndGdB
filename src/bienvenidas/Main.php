649eaa16010e101cce78cdf1ef340075b57d960c
zFrozead0s <141191707+zFrozead0s@users.noreply.github.com>
2023-09-02 11:20:13 +0200

    Actualizar Main.php

--- src/bienvenidas/Main.php
+++ src/bienvenidas/Main.php
@@ -1,33 +1,34 @@
-<?php  
-    
-namespace bienvenidas;
-
-use pocketmine\player\Player;
-use pocketmine\Server;
-use pocketmine\event\player\PlayerQuitEvent;
-use pocketmine\plugin\PluginBase;  
-use pocketmine\event\Listener;  
-use pocketmine\utils\TextFormat;  
-use pocketmine\event\player\PlayerJoinEvent;  
-use pocketmine\utils\Config;
+<?php   
   
-class Main extends PluginBase implements Listener {
-
-  public function onEnable() : void {
-    $this->getServer()->getPluginManager()->registerEvents($this,$this); 
-    $this->getLogger()->info("BienvenidasV2 has been enabled.");  
-    $this->getLogger()->info("This plugin's License is the MIT-License.");   
-  } 
-
-  public function JoinEvent(PlayerJoinEvent $event) {  
-    $player = $event->getPlayer(); 
-    $event->setJoinMessage ("[§a+§r] §b» §l§a" . $player->getName() . ""); 
-  }
+ namespace bienvenidas; 
   
-public function QuitEvent(PlayerQuitEvent $event) {
-    $player = $event->getPlayer(); 
-    $event->setQuitMessage ("[§4-§r] §b» §l§4" . $player->getName() . ""); 
-  }
- 
-}  
+ use pocketmine\player\Player; 
+ use pocketmine\Server; 
+ use pocketmine\event\player\PlayerQuitEvent; 
+ use pocketmine\plugin\PluginBase;   
+ use pocketmine\event\Listener;   
+ use pocketmine\utils\TextFormat;   
+ use pocketmine\event\player\PlayerJoinEvent;   
+ use pocketmine\utils\Config; 
   
+ class Main extends PluginBase implements Listener { 
+  
+   public function onEnable() : void { 
+     $this->getServer()->getPluginManager()->registerEvents($this,$this);  
+     $this->getLogger()->info("BienvenidasV2 has been enabled.");   
+     $this->getLogger()->info("This plugin's License is the MIT-License.");    
+   }   
+  
+   public function JoinEvent(PlayerJoinEvent $event) {   
+     $player = $event->getPlayer();  
+     $name = $player->getName();
+     $event->setJoinMessage ("[§a+§r] §b» §l§a" . $name . "");  
+   } 
+  
+ public function QuitEvent(PlayerQuitEvent $event) { 
+     $player = $event->getPlayer();  
+     $name = $player->getName();
+     $event->setQuitMessage ("[§4-§r] §b» §l§4" . $name . "");  
+   } 
+  
+ }  
