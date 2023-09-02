5fb19fe027cf952dc086c7542455e5859c0dc942
zFrozead0s <141191707+zFrozead0s@users.noreply.github.com>
2023-09-02 11:24:38 +0200

    Actualizar Main.php

--- src/bienvenidas/Main.php
+++ src/bienvenidas/Main.php
@@ -1,34 +1,38 @@
-<?php   
-  
- namespace bienvenidas; 
-  
- use pocketmine\player\Player; 
- use pocketmine\Server; 
- use pocketmine\event\player\PlayerQuitEvent; 
- use pocketmine\plugin\PluginBase;   
- use pocketmine\event\Listener;   
- use pocketmine\utils\TextFormat;   
- use pocketmine\event\player\PlayerJoinEvent;   
- use pocketmine\utils\Config; 
-  
- class Main extends PluginBase implements Listener { 
-  
-   public function onEnable() : void { 
-     $this->getServer()->getPluginManager()->registerEvents($this,$this);  
-     $this->getLogger()->info("BienvenidasV2 has been enabled.");   
-     $this->getLogger()->info("This plugin's License is the MIT-License.");    
-   }   
-  
-   public function JoinEvent(PlayerJoinEvent $event) {   
-     $player = $event->getPlayer();  
-     $name = $player->getName();
-     $event->setJoinMessage ("[§a+§r] §b» §l§a" . $name . "");  
-   } 
-  
- public function QuitEvent(PlayerQuitEvent $event) { 
-     $player = $event->getPlayer();  
-     $name = $player->getName();
-     $event->setQuitMessage ("[§4-§r] §b» §l§4" . $name . "");  
-   } 
-  
- }  
+<?php
+
+namespace bienvenidas;
+
+use pocketmine\player\Player;
+use pocketmine\Server;
+use pocketmine\event\player\PlayerQuitEvent;
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\utils\TextFormat;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\utils\Config;
+
+class Main extends PluginBase implements Listener {
+
+    public function onEnable() : void {
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->getLogger()->info("BienvenidasV2 has been enabled.");
+        $this->getLogger()->info("This plugin's License is the MIT-License.");
+        $this->saveDefaultConfig();
+    }
+
+    public function JoinEvent(PlayerJoinEvent $event) {
+        $player = $event->getPlayer();
+        $name = $player->getName();
+        $msg = $this->getConfig()->get("join_message");
+        $msg = str_replace("{name}", $name, $msg);
+        $event->setJoinMessage($msg);
+    }
+
+    public function QuitEvent(PlayerQuitEvent $event) {
+        $player = $event->getPlayer();
+        $name = $player->getName();
+        $msg = $this->getConfig()->get("quit_message");
+        $msg = str_replace("{name}", $name, $msg);
+        $event->setQuitMessage($msg);
+    }
+}
